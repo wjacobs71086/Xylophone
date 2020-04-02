38ac491d77b2f101e4137b7971e02e6f21fa64a7
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-02 14:11:31 -0700

    finished the boss challange of delay in code"

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,14 +4,22 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        let seconds = 0.2
+        print("Start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            sender.alpha = 1.0
+            print("end")
+        }
     }
+
     
     func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
